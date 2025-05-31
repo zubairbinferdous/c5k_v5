d02f83ed065cd28871bc50ded01428ec3ce50009
Nazmul66 <hnazmul748@gmail.com>
2025-05-31 23:45:41 +0600

    Blog Page and single pages are done without blog sidebar

--- resources/views/pages/all_blogs.blade.php
+++ resources/views/pages/all_blogs.blade.php
@@ -1,37 +1,35 @@
-<title>C5k | All Blogs </title>
-@include('layout.header')
-@section('css')
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
-<style>
-h1, h2, h3, p {
-    font-family: 'Montserrat', sans-serif;
-    font-weight: 400;
-}
-</style>
-@endsection
-<body>
-<div class="container">
-    <h1 class="text-center">All Blogs</h1>
-    @foreach($blogs as $blog)
-    <a href="{{route('blog.details', ['id' => $blog->id])}}">
-    <div class="row mt-2 text-dark" style="border:1px solid #CBCBCB;"> 
-        <div class="col-md-3">
-        <img src="{{ asset('public/backend/blog/' . $blog->image) }}" alt="{{ $blog->title }}" class="img-fluid" style="width: 100%; max-height:200px; margin-Left:-15px;">
-        </div>
-        <div class="col-md-9">
-        <h5 class="mt-2">{{ $blog->title }}</h5>
-        <p class="mt-1">{!! \Illuminate\Support\Str::limit(strip_tags($blog->details), 200, '...') !!}</p>
-        </div>
+@extends('./layouts.master')
+
+@push('add-css')
+
+@endpush
+
+@section('userContent')
+
+    <div class="container mx-auto px-4 py-8">
+        <h1 class="text-3xl font-semibold text-center mb-6">All Blogs</h1>
+    
+        @foreach($blogs as $blog)
+        <a href="{{ route('blog.details', ['id' => $blog->id]) }}" class="block text-gray-800 hover:bg-gray-50 transition rounded-lg border border-gray-300 mb-4">
+            <div class="flex flex-col md:flex-row overflow-hidden">
+                <div class="md:w-1/3">
+                    <img src="{{ asset('public/backend/blog/' . $blog->image) }}"
+                         alt="{{ $blog->title }}"
+                         class="w-full h-52 md:h-full object-cover" />
+                </div>
+                <div class="md:w-2/3 p-4">
+                    <h5 class="text-xl font-bold mb-2">{{ $blog->title }}</h5>
+                    <p class="text-gray-600 text-sm">
+                        {!! \Illuminate\Support\Str::limit(strip_tags($blog->details), 200, '...') !!}
+                    </p>
+                </div>
+            </div>
+        </a>
+        @endforeach
     </div>
-    </a>
-    @endforeach
-</div>
 
+@endsection
 
-@include('layout.footer_nav')
-@include('layout.footer')
-@include('layout.js')
+@push('scripts')
 
-</body>
-
-</html>
+@endpush
--- resources/views/pages/blog_details.blade.php
+++ resources/views/pages/blog_details.blade.php
@@ -1,45 +1,50 @@
-<title>C5k | Details </title>
-@include('layout.header')
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap" rel="stylesheet">
+@extends('./layouts.master')
+
+@push('add-css')
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap" rel="stylesheet">
+
 <style>
-.blog h1, h2, h3 h4 h5 h6{
-  font-family: "Georgia", serif;
-  color:black !important;
 
+    
+    .blog h1, .blog h2, .blog h3, .blog h4, .blog h5, .blog h6 {
+        font-family: 'Merriweather', 'Times New Roman', Georgia, Garamond, serif;
+        color: black !important;
+    }
 
+    .blog {
+        font-family: 'Merriweather', 'Times New Roman', Georgia, Garamond, serif;
+        color: black !important;
+    }
 
-   
-}
-.blog{
-    font-family: 'Merriweather', 'Times New Roman', Georgia, Garamond, serif;
-    text-align: justify;
-    color:black !important;
-
-}
-.blog p{
-    font-family: 'Merriweather', 'Times New Roman', Georgia, Garamond, serif;
-    text-align: justify;
-    color:black !important;
-}
+    .blog p {
+        font-family: 'Merriweather', 'Times New Roman', Georgia, Garamond, serif;
+        text-align: justify;
+        color: black !important;
+    }
 </style>
-<body>
-<div class="container blog">
-<div class="row mt-4">
-        <div class="col-12 col-md-12 col-lg-12">
-            <img src="{{ asset('public/public/backend/blog/' . $blogs->image) }}" alt="{{ $blogs->title }}" class="img-fluid" style="width: 100%; height: auto;">
-            <h3 class="mt-3">{{$blogs->first()->title ??''}}</h3>
-            <p class="mt-2 text-balck text-justify">{!! $blogs->details !!}</p> <!-- Display the full content -->
+
+@endpush
+
+@section('userContent')
+
+<div class="container mx-auto px-4 py-6 blog">
+    <div class="w-full">
+        <img src="{{ asset('public/backend/blog/' . $blogs->image) }}" alt="{{ $blogs->title }}"
+             class="w-full h-auto rounded-lg shadow-sm">
+        
+        <h3 class="mt-6 text-2xl font-bold text-black font-serif">{{ $blogs->first()->title ?? '' }}</h3>
+        
+        <div class="mt-4 text-justify text-black space-y-4 leading-relaxed font-[Merriweather]">
+            {!! $blogs->details !!}
         </div>
+    </div>
 </div>
-</div>
 
+@endsection
 
+@push('scripts')
 
-@include('layout.footer_nav')
-@include('layout.footer')
-@include('layout.js')
-
-</body>
-
-</html>
+@endpush
